cdd3770bc9ec4bf301aea6f84daaeb449c670ea5
Steve Mutuura <stevemurimi44@gmail.com>
2024-09-21 14:28:21 +0300

    Finished the string concatenation for madlipz style song

--- src/strings/App.java
+++ src/strings/App.java
@@ -1,20 +1,36 @@
 package strings;
 
-public class App {
+import java.util.Scanner;
+
+public class  App {
     public static void main(String[] args) {
-        String beginning = " your ";
-        String middle = " gently down the ";
-        String end = "life is but a";
+        Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter a verb: ");
+        String verb = scan.nextLine();
 
         System.out.println("Enter a noun: ");
+        String noun1 = scan.nextLine();
 
         System.out.println("Enter another noun: ");
+        String noun2 = scan.nextLine();
 
         System.out.println("Enter an adverb: ");
+        String adverb = scan.nextLine();
 
         System.out.println("Enter one last noun: ");
+        String noun3 = scan.nextLine();
+
+        String beginning = " your ";
+        beginning =  verb + ", " + verb + ", " + verb + beginning + noun1 + '\n';
+
+        String middle = "gently down the ";
+        middle = middle.concat(noun2 + "." + '\n');
+
+        String end = "life is but a";
+        //end = String.format("%s %s %s %s, %s %s", adverb, adverb, adverb, adverb, end, noun3);
+        end = String.format("%1$s %1$s %1$s %1$s %2$s %3$s%n", adverb, end, noun3);
+        // --> The last %n is for new line. The 1$ is for numbering your variables directly in the format method.
 
         System.out.println("Your Song: ");
         System.out.println(beginning + middle + end);
